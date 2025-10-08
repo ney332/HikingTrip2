c0f9b3f55dec60c3c400b374c55e7a9849b67bf2
Lorran <lorransantos059@gmail.com>
2025-10-08 11:05:40 -0300

    Initial Commit

---
+++ HikingTrip2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HikingTrip2
+//
+//  Created by Lorran Silva on 08/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HikingTrip2/HikingTrip2App.swift
@@ -0,0 +1,17 @@
+//
+//  HikingTrip2App.swift
+//  HikingTrip2
+//
+//  Created by Lorran Silva on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingTrip2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
