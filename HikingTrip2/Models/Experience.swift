c69f1e04e83aaf6ce19324b2d7656c1ee65332db
lorran-mac <lorransantos059@gmail.com>
2025-11-08 01:36:21 -0300

    alt finails

---
+++ HikingTrip2/Models/Experience.swift
@@ -0,0 +1,8 @@
+//
+//  Experience.swift
+//  HikingTrip2
+//
+//  Created by Lorran Silva on 07/11/25.
+//
+
+import Foundation
---
+++ HikingTrip2/Views/Experiencias/ExperienciasView.swift
@@ -0,0 +1,18 @@
+//
+//  ExperienciasView.swift
+//  HikingTrip2
+//
+//  Created by Lorran Silva on 07/11/25.
+//
+
+import SwiftUI
+
+struct ExperienciasView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExperienciasView()
+}
