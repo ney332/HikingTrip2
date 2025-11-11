53c7bda7b7b7f53f205896ffa934f2590bbcd862
lorran-mac <lorransantos059@gmail.com>
2025-11-11 16:29:04 -0300

    parte final do projeto, finalizacao de funcionalidades e inicio de finalização de design

---
+++ HikingTrip2/Views/Experiencias/ExperienceDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ExperienceDetailView.swift
+//  HikingTrip2
+//
+//  Created by Lorran Silva on 08/11/25.
+//
+
+import SwiftUI
+
+struct ExperienceDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExperienceDetailView()
+}
